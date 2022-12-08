47f560b7d68cf80bf612691d552498fd7e0a8f06
mick shannahan <beasticorn@gmail.com>
2022-12-07 23:54:33 -0700

    build for render

---
+++ fight_me/wwwroot/assets/AboutPage.3c35d9c6.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.7b691129.js";import{b as o,d as t,e as s}from"./vendor.3211aab1.js";const a={name:"AboutPage"},n={class:"about"},r=s("h1",null,"This is the about page",-1),c=[r];function _(d,i,p,u,b,f){return o(),t("div",n,c)}var m=e(a,[["render",_]]);export{m as default};
---
+++ fight_me/wwwroot/assets/DashboardPage.4d7f0d00.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.7b691129.js";import"./vendor.3211aab1.js";const a={};function s(e,t,o,n,d,_){return" this is the dashboard "}var p=r(a,[["render",s]]);export{p as default};
